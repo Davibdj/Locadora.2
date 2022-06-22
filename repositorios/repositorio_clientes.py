4382b92973f1264cee6ebb04d218981257a7aa27
João Luis <azjoaoluis7@gmail.com>
2022-06-22 15:02:52 -0300

    22/06/2022

--- repositorios/repositorio_clientes.py
+++ repositorios/repositorio_clientes.py
@@ -4,27 +4,27 @@ from Cliente.cliente import Cliente
 class RepositorioCliente:
 
     def __init__(self):
-        self.clientes = []
+        self.clientes = [] #Inicializando uma lista
 
     def cadastrar(self, cliente: Cliente):
-        if self.buscar(cliente.get_cpf()) is None:
-            self.clientes.append(cliente)
+        if self.buscar(cliente.get_cpf()) is None: #Buscando o cliente pelo número do cpf, e se não for nada:
+            self.clientes.append(cliente)          #Adiciona o cliente a lista cliente
             print("Cadastro Realizado com sucesso!")
         else:
             print("Cliente já cadastrado.")
 
     def buscar(self, cpf):
-        for cliente in self.clientes:
-            if cliente.get_cpf() == cpf:
-                return cliente
+        for cliente in self.clientes:   #Percorre os elementos da lista Clientes
+            if cliente.get_cpf() == cpf: #Verifica se o cpf passado é igual a algum da lista clientes
+                return cliente #Retorna o cliente
             else:
                 return None
 
     def atualizar(self, cliente: Cliente):
-        cliente = self.buscar(cliente.get_cpf())
-        if cliente is not None:
-            cliente.set_nome(cliente.get_nome())
-            cliente.set_endereco(cliente.get_endereco())
+        cliente = self.buscar(cliente.get_cpf())  #Procurando um cliente pelo metodo buscar(verifica o numero do cpf na lista cliente), pelo cpf e atribui a cliente
+        if cliente is not None: # Se o cliente não for nulo então:
+            cliente.set_nome(cliente.get_nome()) #Mudança de nome do cliente
+            cliente.set_endereco(cliente.get_endereco()) #Mudança de endereço do cliente
         else:
             print("Cliente inexistente!")
 
@@ -32,5 +32,5 @@ class RepositorioCliente:
         pass
 
     def listar(self):
-        return self.clientes
+        return self.clientes #Retorna a lista clientes
 
--- repositorios/repositorio_filmes.py
+++ repositorios/repositorio_filmes.py
@@ -3,19 +3,17 @@ from Filmes.filme import Filme
 class RepositorioFilme:
 
     def __init__(self):
-        self.filmes = []
-
+        self.filmes = [] #Inicializa uma lista
 
     def cadastrar(self, filme: Filme):
-        if self.buscar(filme.get_codigo()) is None:
-            self.filmes.append(filme)
+        if self.buscar(filme.get_codigo()) is None: #Procura um filme pelo metodo buscar com o codigo do filme e se ele não for nulo então:
+            self.filmes.append(filme)               #Adiciona um filme a lista filmes
             print("Filme cadastrado com sucesso!")
         else:
             print("Filme já cadastrado.")
 
-
     def buscar(self, codigo):
-        for filme in self.filmes:
+        for filme in self.filmes:                  #
             if filme.get_codigo() == codigo:
                 return filme
             else:
