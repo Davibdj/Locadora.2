a73d6846112e797dcc3d4c9ca65e78ae0be12869
Davi Oliveira <96089988+Davibdj@users.noreply.github.com>
2022-06-24 10:18:28 -0300

    Update repositorio_filmes.py
    add - modificação nos métodos

--- repositorios/repositorio_filmes.py
+++ repositorios/repositorio_filmes.py
@@ -20,10 +20,26 @@ class RepositorioFilme:
                 return None
 
     def atualizar(self, filme):
-        pass
+        filme = self.buscar(filme.get_codigo())  # Procurando um cliente pelo metodo buscar(verifica o numero do cpf na lista cliente), pelo cpf e atribui a cliente
+        if filme is not None:  # Se o cliente não for nulo então:
+            filme.set_titulo(filme.get_titulo())
+            filme.set_codigo(filme.get_codigo())
+            filme.set_sinopse(filme.get_sinopse())
+            filme.set_genero(filme.get_genero())
+            filme.set_diretor(filme.get_diretor())
+            filme.set_produtor(filme.get_produtor())
+            filme.set_preco_locacao(filme.get_preco_locacao())
+            filme.set_atores(filme.get_atores())
+        else:
+            print("Filme inexistente!")
 
     def deletar(self, codigo):
-       pass
+              for filme in self.filmes:
+           if filme.get_codigo() == codigo:
+               self.filmes.pop(self.filmes.index(filme))
+               print("Filme deletado com sucesso!")
+           else:
+               print("Filme não encontrado!")
 
     def listar(self, filme):
-        pass
+        return self.filmes
